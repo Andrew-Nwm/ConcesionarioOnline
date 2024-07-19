164bd51518a678c9d21f5454fdad95a8660ac649
Andrew <162040569+Andrew-Nwm@users.noreply.github.com>
2024-07-18 20:46:15 -0500

    uploading basic structre and possible images will use

---
+++ src/main/java/com/mycompany/concesionarioonline/ConcesionarioOnline.java
@@ -0,0 +1,8 @@
+package com.mycompany.concesionarioonline;
+
+public class ConcesionarioOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Esta clase se encargará de desplegar toda la aplicación.");
+    }
+}
